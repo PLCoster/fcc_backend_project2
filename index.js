653c1b8adda4156fb8c3e1675c3d15c5b980693e
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-08-24 11:05:27 -0700

    Added development request logging, 404 handler and internal error handler to server boilerplate

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-// index.js
-// where your node app starts
-
-// init project
+// Init express app, get env variables if present
 require('dotenv').config();
 var express = require('express');
 var app = express();
+
+// Log incoming requests in development:
+if (process.env.RUN_MODE === 'development') {
+  app.use((req, res, next) => {
+    console.log(
+      `${req.method} ${req.path}; IP=${req.ip}; https?=${req.secure}`,
+    );
+    next();
+  });
+}
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -14,6 +21,7 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+// Return index.html on GET request to '/'
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
@@ -24,7 +32,20 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-// listen for requests :)
+// 404 page not found:
+app.get('*', function (req, res) {
+  // Redirect to index
+  res.redirect('/');
+});
+
+// Internal Error Handler:
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server error: See Server Logs');
+});
+
+// Listen for requests on given PORT, else 3000
+// http://localhost:3000/
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
