3e95edc94bb598512d8a67905dbb669d89d671f1
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-08-24 11:14:16 -0700

    Added /api/whoami route handler to index.js

--- index.js
+++ index.js
@@ -23,17 +23,21 @@ app.use(express.static('public'));
 
 // Return index.html on GET request to '/'
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+// Return request header info on API call
+app.get('/api/whoami', (req, res) => {
+  return res.json({
+    ipaddress: req.ip,
+    language: req.language,
+    software: req.software,
+  });
 });
 
 // 404 page not found:
-app.get('*', function (req, res) {
+app.get('*', (req, res) => {
   // Redirect to index
   res.redirect('/');
 });
@@ -46,6 +50,6 @@ app.use((err, req, res, next) => {
 
 // Listen for requests on given PORT, else 3000
 // http://localhost:3000/
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
